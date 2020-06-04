3580b36b51dfeb2dfa2b7ca2b34314544317aaec
Tennessee Ward <tephilli@ucsc.edu>
2020-06-03 23:39:54 -0500

    new extrudy bopi

---
+++ assets/tilemaps/mapAssetsTilemapExtruded.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="mapAssetsTilemapExtruded" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="156" columns="12">
+ <image source="mapAssetsTilemapExtruded.png" width="216" height="234"/>
+</tileset>
