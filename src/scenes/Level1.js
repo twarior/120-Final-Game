94345851b908184b82f6c6acc7ae57495f044019
Tennessee Ward <tephilli@ucsc.edu>
2020-06-06 23:14:23 -0500

    button press feedbcak

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -61,6 +61,8 @@ class Level1 extends Phaser.Scene {
         this.load.audio('sfx_police3', './assets/sfx/Police3.mp3');
         this.load.audio('sfx_police4', './assets/sfx/Police4.mp3');
         this.load.audio('sfx_police5', './assets/sfx/Police5.mp3');
+        this.load.audio('sfx_button1', './assets/sfx/buttonSelect1.mp3');
+        this.load.audio('sfx_button2', './assets/sfx/buttonSelect2.mp3');
     }
 
 //=====================================================================================================
@@ -88,8 +90,8 @@ class Level1 extends Phaser.Scene {
         const skeletonStatue = map.createStaticLayer('Skeleton_Statue', tileset);
         const groundGhosts = map.createStaticLayer('Ground_Ghost', tileset);
         const reaper = map.createStaticLayer('Reaper', tileset);
-        const button1Door1 = map.createStaticLayer('Button1_Door1', tileset);
-        const button2Door1 = map.createStaticLayer('Button2_Door1', tileset);
+        const button1Door1 = map.createDynamicLayer('Button1_Door1', tileset);
+        const button2Door1 = map.createDynamicLayer('Button2_Door1', tileset);
         
         
         this.skelly = skeletonStatue;
@@ -399,6 +401,8 @@ class Level1 extends Phaser.Scene {
         this.police3 = this.sound.add('sfx_police3').setVolume(7);
         this.police4 = this.sound.add('sfx_police4').setVolume(7);
         this.police5 = this.sound.add('sfx_police5').setVolume(7);
+        this.button1SFX = this.sound.add('sfx_button1');
+        this.button2SFX = this.sound.add('sfx_button2');
         game.sound.volume = .05;
         this.copDeathSounds = [this.police1, this.police2, this.police3, this.police4, this.police5];
         this.ghostDeathSounds = [this.ghostSigh, this.ghostWail];
@@ -768,9 +772,15 @@ class Level1 extends Phaser.Scene {
         if(buttonHit.visible === true && bulletHit.active === true){
             if(buttonHit.layer.name == "Button1_Door1"){
                 this.door1Button1 = true;
+                this.button1SFX.play();
+                buttonHit.setAlpha(.5);
+                //buttonHit.setTint(0x00ff00, 0x00ff00, 0x00ff00, 0x00ff00);
             }
             else if(buttonHit.layer.name == "Button2_Door1"){
                 this.door1Button2 = true;
+                this.button2SFX.play();
+                buttonHit.setAlpha(.5);
+                //buttonHit.setTint(0x00ff00, 0x00ff00, 0x00ff00, 0x00ff00);
             }
             bulletHit.setActive(false).setVisible(false).destroy();
         }
